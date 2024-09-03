cbc80cc07038c65854609d34659fd5f57e52d89c
Felixfailw <98382575+Felixfailw@users.noreply.github.com>
2024-09-03 17:33:39 +0200

    Reorganisation

--- frontend/src/components/WelcomePage.js
+++ frontend/src/components/WelcomePage.js
@@ -11,6 +11,10 @@ function Welcome() {
             <p>Effortlessly Filter and Download Your Calendar Events.</p>
             <p>You can use for example: https://sked.lin.hs-osnabrueck.de/sked/jg/22SPS.ics</p>
             <Link className="get-started-btn" to="/filter">Get Started</Link>
+            <br/>
+            <br/>
+            <p>Disclaimer: The information provided by this tool is for informational purposes only and does not guarantee accuracy or completeness.
+                This tool is not provided, endorsed, or affiliated with HS-Osnabrück. Users should verify the information independently.</p>
         </div>
         </>
     );
