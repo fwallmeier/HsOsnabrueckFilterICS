2790965ca3795e2f937dd08b4381c9203d45cceb
Felixfailw <98382575+Felixfailw@users.noreply.github.com>
2024-09-03 17:11:41 +0200

    Reorganisation

--- frontend/src/components/WelcomePage.js
+++ frontend/src/components/WelcomePage.js
@@ -9,6 +9,7 @@ function Welcome() {
             <img src={`${process.env.PUBLIC_URL}/Logo.svg`} alt="Hochschule Osnabrück Logo" className="hs-logo" />
             <h1>Welcome to the ICS File Filter Tool</h1>
             <p>Effortlessly Filter and Download Your Calendar Events.</p>
+            <p>You can use for example: https://sked.lin.hs-osnabrueck.de/sked/jg/22SPS.ics</p>
             <Link className="get-started-btn" to="/filter">Get Started</Link>
         </div>
         </>
